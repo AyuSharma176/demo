2e1b1ea07733ad935519cac86fe68464f6074252
Komal <rastogikomal57@gmail.com>
2025-08-07 14:51:43 +0530

    addition added

---
+++ calculator/calculator.java
@@ -0,0 +1,9 @@
+
+public class calculator {
+    public static void main(String[] args) {
+      int res =  addition(n,m);
+}
+    public static int addition(int a, int b){
+        return a+b;
+    }
+}
